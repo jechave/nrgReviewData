5dde81d5259994375c46c94f1602de883db1a2f5
Claus Wilke <wilke@austin.utexas.edu>
2015-09-10 16:33:21 -0500

    basic framework for pdb images done

--- structures/color_script.py
+++ structures/color_script.py
@@ -2,13 +2,14 @@
 
 
 def apply_color(pdb, data):
+    print pdb, data
     cmd.load("%s.pse"%pdb)
     if data == "WCN":
         max = -2.5
         min = 2.5
     elif data == "rate":
-        max = 4
-        min = 0
+        max = -2.5
+        min = 2.5
     else:
         max = 1
         min = 0
@@ -40,19 +41,25 @@ def apply_color(pdb, data):
     cmd.bg_color(color="white")
 
 
-
-
-pdb = '1AKO'
-ray = False
-output = True
-
-#for data in ('WCN', 'RSA', 'rate'):
-#for data in ('WCN', 'RSA'):
-for data in ('RSA', 'WCN'):
+def make_figure(pdb, data, ray, output):
     apply_color(pdb, data)
 
     if ray:
-        cmd.ray("1600", "1200")
+        cmd.ray("1200", "900")
     if output:
         cmd.png("%s-%s.png" % (pdb, data) [0:4])
+
+
+pdb_list = ['1AKO', '1R44', '1OGO', '1LVH']
+#pdb_list = ['1R44']
+
+data_list = ['WCN', 'RSA', 'rate']
+#data_list = ['rate']
+
+ray = False
+output = True
+
+for pdb in pdb_list:
+    for data in data_list:
+        make_figure(pdb, data, ray, output)
 
